f5f7a5ad2ae6ee0116386f9d6465f8f482b7e0f1
Amit Kumar <amitkh@gmail.com>
2022-03-20 09:12:44 -0400

    added freq and cli

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import bz2
 import pickle
+import os.path
 
 def compress_pickle(name, data):
     with bz2.BZ2File(name, 'w') as f:
@@ -9,3 +10,27 @@ def decompress_pickle(name):
     data = bz2.BZ2File(name, 'rb')
     data = pickle.load(data)
     return data
+
+def reg_pickle(name, data):
+    with open(name, 'wb') as f:
+        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def get_pickle(name):
+    with open(name, 'rb') as f:
+        data = pickle.load(f)
+    return data
+        
+def get_words():
+    with open('words.txt', 'r') as f:
+        words = list(i.strip() for i in f.readlines())
+    return words
+
+def get_pattern_dict():
+    if os.path.exists('patterns.pbz2'):
+        typer.echo('Loading compressed pattern dictionary (may take up to two minutes)...')
+        pattern_dict = decompress_pickle('patterns.pbz2')
+    elif os.path.exists('patterns.p'):
+        pattern_dict = get_pickle('patterns.p')
+    else:
+        raise FileNotFoundError(f'Pattern dictionary not found. If you haven\'t, you can generate a pattern dicitonary by running wordle gen_pattern_dict.')
+    return pattern_dict
--- wordle.py
+++ wordle.py
@@ -6,20 +6,40 @@ from tqdm import tqdm
 import utils
 import numpy as np
 import json
+import typer
+import os.path
+from typing import List
+import pprint
 
+app = typer.Typer()
 PATTERNS = list(itertools.product(range(3), repeat=5))
-use_tares = False
 
-# TODO: Incorporate word weights and scoring. Make the CLI. Translate in Spanish
+# TODO: Incorporate word weight scoring. Translate in Spanish
+
 def get_sorted_words():
-    with open('words.txt', 'r') as f:
-        words = list(i.strip() for i in f.readlines())
-    
+    '''
+    Sorts all words in word list by frequency according to wordfreq module
+    '''
+    words = utils.get_words()
     raw_freqs = {word: freq(word, 'en') for word in words}
     sorted_words = sorted(raw_freqs, key= lambda x: raw_freqs[x])
     return sorted_words
     
 def gen_weights(width=10, cutoff=3000):
+    '''
+    Calculate probability of each word in the wordlist of being a wordle answer.
+    This calculation is done by applying a sigmoid function with the
+    cutoff being the 3000 most frequent words.
+
+    Args:
+        width (int): the width on which to place the words (in other words, the
+                sharpness of the sigmoid transition
+
+        cutoff (int): The midpoint of the function where the probability is 1/2
+
+    Returns:
+        weights (dict): dictionary mapping each word to the probability
+    '''
     sigmoid = lambda x: 1/(1 + np.exp(-x))
     sorted_words = get_sorted_words()
     num_words = len(sorted_words)
@@ -60,7 +80,8 @@ def make_pattern(guess, ans):
             pattern.append(0)
     return tuple(pattern)
 
-def gen_pattern_dict(words):
+@app.command()
+def gen_pattern_dict(compress: bool=typer.Option(True, help='Compress patterns file from 800MB to 200MB at the expense of read/write time')):
     '''
     Generate dictionary where each word in possible answers is a key, and each
     value is another dictionary. The inner dictionary's keys are all of the
@@ -75,31 +96,54 @@ def gen_pattern_dict(words):
         pattern_dict (dict): Dictionary mapping all patterns and words to
             remaining candidates
     '''
+    words = utils.get_words()
     pattern_dict = defaultdict(lambda: defaultdict(set))
     for word in tqdm(words):
         for word2 in words:
             pattern = make_pattern(word, word2)
             pattern_dict[word][pattern].add(word2)
-    return dict(pattern_dict)
+    pattern_dict = dict(pattern_dict)
+    if compress:
+        utils.compress_pickle('patterns.pbz2', pattern_dict)
+    else:
+        utils.reg_pickle('patterns.p', pattern_dict)
 
 def calc_entropies(guesses, rem_words, pattern_dict, weights):
+    '''
+    Calculate dictionary of entropy in pattern distribution for each possible
+    guess given rem_words remaining words with the probability for each word
+    being a Wordle answer in weights.
+
+    Args:
+        guesses (set): list of all possible guesses
+        rem_words (set): list of all remaining answer candidates
+        pattern_dict (dict): dictionary connecting words and patterns, created
+            by gen_pattern_dict()
+        weights (dict): dictionary mapping each word to probability of being an
+            answer
+
+    Returns:
+        entropies (dict): Maps each potential guess to entropy of it's pattern
+            distribution
+    '''
     entropies = {}
     for guess in guesses:
         # Calculate entropy of distribution for how many possible remaining words will exist from all 243 patterns
         cnt = []
         for pattern in PATTERNS:
             matches = pattern_dict[guess][pattern]
-            cnt.append(sum(weights[match] for match in matches.intersection(rem_words)))
+            matches = matches.intersection(rem_words)
+            cnt.append(sum(weights[match] for match in matches)/len(rem_words))
         entropies[guess] = entropy(cnt)
     return entropies
 
-def play_game(ans, words, pattern_dict, weights):
+def play_game(ans, words, pattern_dict, weights, savetime=True):
     words = set(words)
     rem_words = words.copy()
     init = 1
 
     # Save time for bulk calculations since the first guess will always be the same
-    if use_tares:
+    if savetime:
         guess = 'tares'
         pattern = make_pattern(guess, ans)
         rem_words = rem_words.intersection(pattern_dict[guess][pattern])
@@ -119,48 +163,98 @@ def play_game(ans, words, pattern_dict, weights):
         rem_words = rem_words.intersection(pattern_dict[guess][pattern])
         
         # Some console output
-        if not use_tares:
-            print(f'Guess #{i}: {guess}\nGiven info {pattern}\nExp. Entropy: {round(entropies[guess], 3)}')
+        if not savetime:
+            typer.echo(f'Guess #{i}: {guess}')
+            typer.echo(f'Given info {pattern}')
+            typer.echo(f'Exp. Entropy: {round(entropies[guess], 3)}')
             if len(rem_words) < 100:
-                print(rem_words)
+                typer.echo(rem_words)
 
         if guess == ans:
+
             # Game is won
-            print(f'Guessed {ans} in {i} guesses')
+            typer.echo(f'Guessed {ans} in {i} guesses')
             break
 
     return i
 
-def test_all_answers(of_name, gen=False):
+@app.command()
+def test_all(of_name: str=typer.Argument('stats.json', help='Where to save results')):
+    '''
+    Run solver on all 2315 original wordle answers, writes JSON file with num
+    of guesses for each word
+
+    Args:
+        of_name (str): where to write test results
+    '''
     # Loading text files
-    with open('words.txt', 'r') as f:
-        words = list(i.strip() for i in f.readlines())
+    words = utils.get_words()
+
     with open('solutions.txt', 'r') as sol:
         solutions = list(i.strip() for i in sol.readlines())
 
-    # Generating or loading pattern_dict
-    if gen:
-        pattern_dict = wordle.gen_pattern_dict(words)
-        utils.compress_pickle('patterns.pbz2', pattern_dict)
-    else:
-        print('Loading pattern dictionary (may take up to two minutes)...')
-        pattern_dict = utils.decompress_pickle('patterns.pbz2')
+    # Loading pattern_dict
+    pattern_dict = utils.get_pattern_dict()
+    weights = gen_weights()
 
     # Playing all 2315 possible games
     stats = {}
     for sol in tqdm(solutions):
-        stats[sol] = wordle.play_game(sol, words, pattern_dict)
+        stats[sol] = wordle.play_game(sol, words, pattern_dict, weights)
 
     # Writing output to JSON
     with open(of_name, 'w') as of:
         json.dump(stats, of)
 
-REMAP_COLORS = {0: "⬛", 1: "🟨", 2: "🟩"}
-def emojify(pattern):
-    emoji = ''
-    for char in pattern:
-        emoji += REMAP_COLORS[char]
-    return emoji
+@app.command()
+def play_words(answers: List[str]):
+    '''
+    Simlulate a list of Wordle words
+    '''
+    words = utils.get_words()
+    pattern_dict = utils.get_pattern_dict()
+    weights = gen_weights()
+    for ans in answers:
+        play_game(ans, words, pattern_dict, weights, False)
 
+@app.command()
+def play():
+    typer.echo('Loading wordlist...')
+    words = utils.get_words()
+    typer.echo('Loading pattern dictionary (may take up to two minutes)...')
+    pattern_dict = utils.get_pattern_dict()
+    typer.echo('Generating frequency weights...')
+    weights = gen_weights()
+
+    words = set(words)
+    rem_words = words.copy()
+
+    for i in range(1, 7):
+        entropies = calc_entropies(words, rem_words, pattern_dict, weights)
+        entropies = sorted(list(entropies.items()), reverse=True, key=lambda x: x[1])
+        if len(rem_words) > 1:
+            typer.echo(f'{len(rem_words)} words are remaining. Top guesses are:')
+            typer.echo(pprint.pformat(entropies[:10]))
+        guess = typer.prompt('Guess:\n>')
+
+        # Reduce the remaining words to those that match the new pattern 
+        pattern = typer.prompt('What was your pattern? (Green=2, Yellow=1, Grey=0)\n>')
+        pattern = tuple(int(i) for i in pattern)
+        rem_words = rem_words.intersection(pattern_dict[guess][pattern])
+        
+        if pattern == (2, 2, 2, 2, 2):
+            typer.echo(f'Congratulations! You won in {i} guesses')
+            raise typer.Exit()
+
+        if len(rem_words) < 20:
+            typer.echo('Remaining words are:')
+            typer.echo(pprint.pformat([(word, weights[word]) for word in rem_words]))
+
+    raise typer.Exit()
+
+
+
+if __name__ == '__main__':
+    app()
         
     
